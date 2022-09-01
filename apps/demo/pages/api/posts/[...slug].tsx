17fe469011f360b1e97b36f1796c95d3bc0f2f51
Vyacheslav Goreev <v.goreev@innopolis.ru>
2022-09-01 12:31:54 +0300

    feat(demo): Readdir in a separate function

--- apps/demo/pages/api/posts/[...slug].tsx
+++ apps/demo/pages/api/posts/[...slug].tsx
@@ -3,14 +3,19 @@ import {resolve} from 'path';
 
 import {NextApiRequest, NextApiResponse} from 'next';
 
-export default async function getPosts(req: NextApiRequest, res: NextApiResponse) {
+export const getPosts = () => {
+  return fs.readdirSync(resolve(process.cwd(), 'assets/posts'));
+
+}
+
+
+export default async function(req: NextApiRequest, res: NextApiResponse) {
   const { method } = req;
 
   switch (method) {
     case 'GET':
       try {
-        const posts = fs.readdirSync(resolve(process.cwd(), 'assets/posts'));
-        res.status(200).json(posts);
+        res.status(200).json(getPosts());
       } catch (e) {
         res.status(500).json(e);
       }
