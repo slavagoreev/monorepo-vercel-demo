562b9001488d13c496b18350209e29cc94baa762
Vyacheslav Goreev <v.goreev@innopolis.ru>
2022-09-01 12:23:22 +0300

    feat(demo): Monorepo readdir bug

---
+++ apps/demo/pages/api/posts/[...slug].tsx
@@ -0,0 +1,21 @@
+import fs from 'fs';
+import {resolve} from 'path';
+
+import {NextApiRequest, NextApiResponse} from 'next';
+
+export default async function getPosts(req: NextApiRequest, res: NextApiResponse) {
+  const { method, query } = req;
+
+  switch (method) {
+    case 'GET':
+      try {
+        const posts = fs.readdirSync(resolve(process.cwd(), 'assets/posts'));
+        res.status(200).json(posts);
+      } catch (e) {
+        res.status(500).json(e);
+      }
+      break;
+    default:
+      res.status(405).end(`Method ${method} Not Allowed`);
+  }
+}
